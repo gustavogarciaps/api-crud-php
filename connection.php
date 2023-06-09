0d27a9b54e1718bf168ce3b45c3c13129bdd8df5
gustavo-garcia-de-sousa <sousagustavogarcia@gmail.com>
2023-06-09 13:07:41 -0300

    ajustes

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 /*-------- CONFIGURAÇÕES DO SERVIDOR -------- */
 $hostname = '127.0.0.1';
-$username = 'id20658733_root';
-$password = '_}>Ms~0)+Aort739';
-$database = 'id20658733_appinventor';
+$username = '';
+$password = '';
+$database = '';
 $sql = "mysql:host={$hostname};dbname={$database};";
 $dsn_Options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
 /*-------- CONFIGURAÇÕES DO SERVIDOR -------- */
